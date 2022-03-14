6868afb8ac764301e0284876b3d434a6b56c4be5
ngranzow <nate.granzow@gmail.com>
2022-03-13 23:32:03 -0500

    added comments

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -5,16 +5,19 @@ const { v4: uuidv4 } = require("uuid");
 const notes = require('../../db/db');
 const { createNewNote, findById, deleteNote } = require('../../lib/notes');
 
+// get notes saved in db.json
 router.get('/notes', (req, res) => {
     res.json(notes);
 });
 
+// create new notes to be saved in db.json
 router.post('/notes', (req, res) => {
     req.body.id = uuidv4();
     createNewNote(req.body, notes);
     res.json(req.body);
 });
 
+// delete notes from db.json based on id
 router.delete('/notes/:id', (req, res) => {
     const note = findById(req.params.id, notes);
 
