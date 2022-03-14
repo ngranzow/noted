de2c60abfb4b42d88e5d794f3684a92591719147
ngranzow <nate.granzow@gmail.com>
2022-03-13 21:13:20 -0500

    cannot get note to save going to try something else

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,21 +1,15 @@
 const router = require("express").Router();
 const { notes } = require('../../db/db');
-const { createNewNote, findById, editNote, deleteNote } = require('../../lib/notes');
+const { createNewNote, findById, deleteNote } = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
     res.json(notes);
 });
 
 router.post('/notes', (req, res) => {
-    if (!req.body.id) {
-        req.body.id = notes.length.toString();
-        let note = createNewNote(req.body, notes);
-        res.json(note);
-    } else {
-        req.body.id = notes.length.toString();
-        let note = editNote(req.body, notes);
-        res.json(note);
-    }
+    req.body.id = notes.length.toString();
+    let note = createNewNote(req.body, notes);
+    res.json(note);
 });
 
 router.delete('/notes/:id', (req, res) => {
--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const htmlRoutes = require('./routes/htmlRoutes');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static('public'));
+
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
-
-app.use(express.static('public'));
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
